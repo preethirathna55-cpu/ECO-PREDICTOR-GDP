0a7e9f0783f2b8e4e273e3da354a7e694cca7962
preethirathna55-cpu <preethirathna55@gmail.com>
2026-04-24 00:40:17 +0530

    app.py

--- app.py
+++ app.py
@@ -102,20 +102,10 @@ elif worst == "Education":
 elif worst == "Health":
     st.write("Improve healthcare")
 
-# ---------------- PREDICTION ----------------
-st.subheader("🤖 Predict GDP")
+data = np.array([[inflation, unemployment, life_exp]])
+data = sm.add_constant(data)
 
-inflation = st.number_input("Inflation")
-unemployment = st.number_input("Unemployment")
-life_exp = st.number_input("Life Expectancy")
-education = st.number_input("Education")
-gov = st.number_input("Gov Spending")
-investment = st.number_input("Investment")
-trade = st.number_input("Trade")
-pop = st.number_input("Population Growth")
-
-if st.button("Predict GDP"):
-    data = np.array([[inflation, unemployment, life_exp, education, gov, investment, trade, pop]])
-    pred = model.predict(data)
+pred = model.predict(data)
+st.success(f"💰 Predicted GDP: {pred[0]:.2f}")
     st.success(f"Predicted GDP: {pred[0]:.2f}")
 
